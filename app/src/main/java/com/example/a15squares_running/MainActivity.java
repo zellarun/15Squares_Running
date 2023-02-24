bceecd13c1b30e56cc0fd9be07ab236d73b90659
zellarun <running26@up.edu>
2023-02-24 14:45:31 -0800

    game with all files no confetti

--- app/src/main/java/com/example/a15squares_running/MainActivity.java
+++ app/src/main/java/com/example/a15squares_running/MainActivity.java
@@ -7,10 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-/*TITLE:
-  PURPOSE:
-  DATE:
-  AUTHOR:
+/*TITLE: main
+  PURPOSE: this sets the start button to go to game page when clicked
+  DATE: 24 February 2023
+  AUTHOR: Zella Running
 */
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonStartGame = this.<Button>findViewById(R.id.buttonStart);
+        //makes the start button clickable
+        Button buttonStartGame = this.findViewById(R.id.buttonStart);
         buttonStartGame.setOnClickListener(this::onClick);
 
     }
 
+    //this method makes it so when the start button is clicked then it will take user to game class
     private void onClick(View view) {
         Intent intent = new Intent(MainActivity.this, game.class);
         startActivity(intent);
